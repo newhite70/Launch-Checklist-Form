bd400ae9503edd9d574977ad2e346d21e5d5ff2e
Initial Commit <newhite7070@gmail.com>
2020-05-27 10:25:30 -0400

    Submission

--- script.js
+++ script.js
@@ -1,47 +1,33 @@
-// Write your JavaScript code here!
-
-/* This block of code shows how to format the HTML once you fetch some planetary JSON!
-<h2>Mission Destination</h2>
-<ol>
-   <li>Name: ${}</li>
-   <li>Diameter: ${}</li>
-   <li>Star: ${}</li>
-   <li>Distance from Earth: ${}</li>
-   <li>Number of Moons: ${}</li>
-</ol>
-<img src="${}">
-*/
-// req 1 is complete
-
-
-
 let planet;
+//Fetches the Json and displays random planet on load.  Also Satifies the Bonus
 fetch('https://handlers.education.launchcode.org/static/planets.json')
 .then(function (response) {
     response.json().then(function (json) {
        planet = json;
-        for (let i = 0; i < planet.length; i++) {
-            destination.innerHTML += `<option value="${i}">${planet[i].name}</option>`;
-        }
+       let randomPlanet = Math.floor(Math.random() * 6);
+       missionTarget.innerHTML = `
+       <h2>Mission Destination</h2>
+       <ol>
+          <li>Name: ${planet[randomPlanet].name}</li>
+          <li>Diameter: ${planet[randomPlanet].diameter}</li>
+          <li>Star: ${planet[randomPlanet].star}</li>
+          <li>Distance from Earth: ${planet[randomPlanet].distance}</li>
+          <li>Number of Moons: ${planet[randomPlanet].moons}</li>
+       </ol>
+    <img src="${planet[randomPlanet].image}">`;
     });
 });
 
+// Event listenter waiting for submission of form to execuete theMeat function.
 window.addEventListener("load", function() {
-   console.log("load")
-   // faultyItems.style.visibility = 'visible';
-   
-//  let form = document.querySelector("form");
 formSubmit.addEventListener("click", theMeat );
-destination.addEventListener('change', theMeat);  
-   // faultyItems.style.visibility = 'visible';
-   
 });
 
 
 function theMeat(event){
-   event.preventDefault();
-   console.log("formsub")
    
+   event.preventDefault();
+
    let minFuel = 10000;
    let maxCargo = 10000;
    let pilotName = document.querySelector("input[name=pilotName]").value;
@@ -49,26 +35,6 @@ function theMeat(event){
    let fuelLevel = document.querySelector("input[name=fuelLevel]").value;
    let cargoMass = document.querySelector("input[name=cargoMass]").value;
 
-   //destination validation and display
-   if (destination.value === '') {
-      alert("Please select a destination")
-      launchStatus.innerHTML = 'Shuttle must have destination for lauch.';
-      launchStatus.style.color = 'red';
-      return;
-   }
-   else{
-   missionTarget.innerHTML = `
-      <h2>Mission Destination</h2>
-         <ol>
-            <li>Name: ${planet[destination.value].name}</li>
-            <li>Diameter: ${planet[destination.value].diameter}</li>
-            <li>Star: ${planet[destination.value].star}</li>
-            <li>Distance from Earth: ${planet[destination.value].distance}</li>
-            <li>Number of Moons: ${planet[destination.value].moons}</li>
-         </ol>
-      <img src="${planet[destination.value].image}">`;
-   }
-   
    //pilotName validation and not a num
    for (let i = 0; i < pilotName.length; i++){
       if(isNaN(pilotName[i]) === false){
@@ -132,7 +98,7 @@ function theMeat(event){
       cargoStatus.innerHTML = 'Cargo mass low enough for launch.';
    }
    // check shuttle status
-   if (fuelLevel > minFuel && cargoMass < maxCargo) {
+   if (fuelLevel >= minFuel && cargoMass <= maxCargo) {
       launchStatus.innerHTML = 'Shuttle is ready for launch.';
       launchStatus.style.color = 'green';
       pilotStatus.innerHTML = pilotName + ' is ready.';
@@ -146,24 +112,4 @@ function theMeat(event){
    document.getElementById('copilotStatus').innerHTML = copilotName + ' is ready.';
    faultyItems.style.visibility = 'visible';
    }
-
-
-   // const launchStatusCheck = document.getElementById("launchStatusCheck");
-   // let x = "";
-    
-   //   x += launchStatusCheck.innerHTML = `
-   //       <div >
-   //           <div >
-   //               <h3>${json[i].firstName} ${json[i].lastName}</h3>
-   //                   <ol>
-   //                       <li>${pi}</li>
-   //                       <li>Active: ${json[i].active}</li>
-   //                       <li>Skills: ${json[i].skills}</li>
-   //                   </ol>
-   //               </div>
-   //           <img class = "avatar" src=${json[i].picture}>
-   //       </div> `;
-   //       document.getElementById("launchStatusCheck").innerHTML = x;
-   //       }});
-   //   });
- };
+};
